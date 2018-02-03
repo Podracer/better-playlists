8c7e798f7592f5ba59a4169ca12418c639ab3ffb
Steven Horrocks <horrockssteven@hotmail.com>
2018-02-03 19:13:56 +0000

    Cleaned Up Stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-let defaulttTextColor = '#ff1212';
+
 let defaultStyle = {
-  color: defaulttTextColor
-}
+  color:  '#fff'
+};
 
 
   class Aggregate extends Component {
@@ -45,7 +44,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Title</h1>  
+        <h1 style={{...defaultStyle,'font-size': '54px'}}>Title</h1>  
         <Aggregate/>
         <Aggregate/>
         <Filter/>
